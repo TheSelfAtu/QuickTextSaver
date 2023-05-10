2c9e6986416f5ea2dfdffbcfb988418e26f2dd6b
TheSelfAtu <TheSelfAtu@example.com>
2023-05-10 23:34:06 +0900

    Implement multiple template storage and copying
    This commit introduces two major changes:
    
    1. Multiple Template Storage: The extension can now store multiple templates, as opposed to just one in the previous version. The templates are stored as an array in the local storage.
    
    2. Copying Templates: Clicking or pressing 'Enter' on any of the stored templates now copies the template to the clipboard. This allows for easy use of the templates in other applications.

--- src/popup.ts
+++ src/popup.ts
@@ -1,39 +1,38 @@
+function addTemplateItem(template: string) {
+  const templateList = document.getElementById("templateList");
+  const item = document.createElement("button");
+  item.textContent = template;
+  item.addEventListener("click", async () => {
+    try {
+      await navigator.clipboard.writeText(template);
+      alert("Template copied to clipboard");
+    } catch (err) {
+      console.error("Failed to copy template to clipboard: ", err);
+    }
+  });
+  templateList?.appendChild(item);
+}
+
 document.getElementById("saveTemplate")?.addEventListener("click", () => {
   const templateEditor = document.getElementById(
     "templateEditor"
   ) as HTMLTextAreaElement;
   const template = templateEditor.value;
   if (template) {
-    chrome.storage.local.set({ savedTemplate: template }, () => {
-      alert("Template saved");
+    chrome.storage.local.get("savedTemplates", (data) => {
+      const templates = data.savedTemplates || [];
+      templates.push(template);
+      chrome.storage.local.set({ savedTemplates: templates }, () => {
+        alert("Template saved");
+        addTemplateItem(template); // Add new item to list
+      });
     });
   }
 });
 
-chrome.storage.local.get("savedTemplate", (data) => {
-  const templateEditor = document.getElementById(
-    "templateEditor"
-  ) as HTMLTextAreaElement;
-  if (data.savedTemplate) {
-    templateEditor.value = data.savedTemplate;
-  }
-});
-
-document
-  .getElementById("applyTemplate")
-  ?.addEventListener("click", async () => {
-    // Async keyword added
-    const templateEditor = document.getElementById(
-      "templateEditor"
-    ) as HTMLTextAreaElement;
-    const template = templateEditor.value;
-    if (template) {
-      try {
-        await navigator.clipboard.writeText(template); // Copy template to clipboard
-        alert("Template copied to clipboard"); // Notify user
-        window.close();
-      } catch (err) {
-        console.error("Failed to copy template to clipboard: ", err);
-      }
-    }
+chrome.storage.local.get("savedTemplates", (data) => {
+  const templates = data.savedTemplates || [];
+  templates.forEach((template: string) => {
+    addTemplateItem(template); // Add each item to list
   });
+});
