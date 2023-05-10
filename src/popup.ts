52f6e9452ca051839a4e362b4bbf534622dbcc1b
TheSelfAtu <TheSelfAtu@example.com>
2023-05-10 23:49:39 +0900

    Refactor popup.ts for improved readability and maintainability
    This commit includes the following improvements:
    
    1. Abstracted out the functionalities into separate functions - `copyToClipboard`, `createTemplateItem`, and `saveTemplate`. This makes the code more readable and easier to maintain.
    
    2. Grouped the DOM element retrievals at the beginning of the script. This centralizes the references to DOM elements, reducing the possibility of errors.

--- src/popup.ts
+++ src/popup.ts
@@ -1,38 +1,45 @@
-function addTemplateItem(template: string) {
-  const templateList = document.getElementById("templateList");
-  const item = document.createElement("button");
-  item.textContent = template;
-  item.addEventListener("click", async () => {
-    try {
-      await navigator.clipboard.writeText(template);
-      alert("Template copied to clipboard");
-    } catch (err) {
-      console.error("Failed to copy template to clipboard: ", err);
-    }
-  });
-  templateList?.appendChild(item);
+async function copyToClipboard(text: string) {
+  try {
+    await navigator.clipboard.writeText(text);
+    alert("Template copied to clipboard");
+  } catch (err) {
+    console.error("Failed to copy template to clipboard: ", err);
+  }
 }
 
-document.getElementById("saveTemplate")?.addEventListener("click", () => {
-  const templateEditor = document.getElementById(
-    "templateEditor"
-  ) as HTMLTextAreaElement;
+function createTemplateItem(template: string) {
+  const item = document.createElement("button");
+  item.textContent = template;
+  item.addEventListener("click", () => copyToClipboard(template));
+  return item;
+}
+
+function saveTemplate(template: string) {
+  chrome.storage.local.get("savedTemplates", (data) => {
+    const templates = data.savedTemplates || [];
+    templates.push(template);
+    chrome.storage.local.set({ savedTemplates: templates }, () => {
+      templateList?.appendChild(createTemplateItem(template));
+    });
+  });
+}
+
+const templateEditor = document.getElementById(
+  "templateEditor"
+) as HTMLTextAreaElement;
+const saveTemplateButton = document.getElementById("saveTemplate");
+const templateList = document.getElementById("templateList");
+
+saveTemplateButton?.addEventListener("click", () => {
   const template = templateEditor.value;
   if (template) {
-    chrome.storage.local.get("savedTemplates", (data) => {
-      const templates = data.savedTemplates || [];
-      templates.push(template);
-      chrome.storage.local.set({ savedTemplates: templates }, () => {
-        alert("Template saved");
-        addTemplateItem(template); // Add new item to list
-      });
-    });
+    saveTemplate(template);
   }
 });
 
 chrome.storage.local.get("savedTemplates", (data) => {
   const templates = data.savedTemplates || [];
   templates.forEach((template: string) => {
-    addTemplateItem(template); // Add each item to list
+    templateList?.appendChild(createTemplateItem(template));
   });
 });
